1be3d2b34d91694c9173cccb6d34f61f8c575869
awlada <awlada@mail.csuchico.edu>
2019-11-13 23:26:55 -0800

    Lab 24

--- main.cpp
+++ main.cpp
@@ -1,25 +1,66 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 // <-- ADD YOUR FUNCTION PROTOTYPE HERE
+int trioValues (int Red, int Green, int Blue);
+int comparator(const void *p, const void *q)
+{
+int l = *(const int *)p;
+int r = *(const int *)q;
+
+if ((l&l) && (r&l))
+        return (r-1);
+
+if (!(l&l) && !(r&l))
+        return (l-r);
+
+if (!(l&l))
+        return l;
+
+return -l;
+}
 
 int main()
 {
-  //DO NOT CHANGE WITHIN THIS AREA...
-  int red, blue, green;
-  cout<<"Enter Red, Green, and Blue values: ";
-  cin>>red>>green>>blue;
-  //...END OF "DO NOT CHANGE" AREA
+        //DO NOT CHANGE WITHIN THIS AREA...
+        int red, blue, green;
+        cout<<"Enter Red, Green, and Blue values: ";
+        cin>>red>>green>>blue;
+        //...END OF "DO NOT CHANGE" AREA
 
 
-  // <-- ADD YOUR FUNCTION CALL HERE
+        // <-- ADD YOUR FUNCTION CALL HERE
+        trioValues( red, green, blue);
 
+      /*  //DO NOT CHANGE WITHIN THIS AREA...
+        cout<<"Rearranged....\n";
+        cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
+        return 0;
+        //...END OF "DO NOT CHANGE" AREA
+      */
 
-  //DO NOT CHANGE WITHIN THIS AREA...
-  cout<<"Rearranged....\n";
-  cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
-  return 0;
-  //...END OF "DO NOT CHANGE" AREA
 }
 
 // <-- ADD YOUR FUNCTION DEFINITON HERE
+int trioValues (int Red, int Green, int Blue)
+{
+   /* vector<int> numbers;
+
+    numbers.push_back (Red);
+    numbers.push_back (Green);
+    numbers.push_back (Blue);
+*/
+    int arr[] = {Red, Green, Blue};
+
+    int size = sizeof(arr)/sizeof(arr[0]);
+    qsort((void*)arr, size, sizeof(arr[0]), comparator);
+    Red = arr[2];
+    Green = arr[1];
+    Blue = arr[0];
+
+    cout<<"Rearragned....\n";
+    cout<<"RGB: "<<Red<<","<<Green<<","<<Blue<<endl;
+
+}
