4736b7c4eec1d3ff65001e4b0cee6ec458f0fa63
awlada <awlada@mail.csuchico.edu>
2019-11-15 16:32:13 -0800

    sorting is weird and doesn't put them in order

--- main.cpp
+++ main.cpp
@@ -4,7 +4,43 @@
 using namespace std;
 
 // <-- ADD YOUR FUNCTION PROTOTYPE HERE
-int trioValues (int Red, int Green, int Blue);
+//int trioValues (int& Red, int& Green, int& Blue);
+int trioValues (int& Red, int& Green, int& Blue);
+int comparator(const void *, const void *);
+int main()
+{
+        //DO NOT CHANGE WITHIN THIS AREA...
+        int red, blue, green;
+        cout<<"Enter Red, Green, and Blue values: ";
+        cin>>red>>green>>blue;
+        //...END OF "DO NOT CHANGE" AREA
+
+
+        // <-- ADD YOUR FUNCTION CALL HERE
+        trioValues( red, green, blue);
+
+        //DO NOT CHANGE WITHIN THIS AREA...
+        cout<<"Rearranged....\n";
+        cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
+        return 0;
+        //...END OF "DO NOT CHANGE" AREA
+      
+
+}
+
+// <-- ADD YOUR FUNCTION DEFINITON HERE
+//int trioValues (int& Red, int& Green, int& Blue)
+int trioValues (int& Red, int& Green, int& Blue)
+{
+    int arr[] = {Red, Green, Blue};
+
+    int size = sizeof(arr)/sizeof(arr[0]);
+    qsort((void*)arr, size, sizeof(arr[0]), comparator);
+    Red = arr[2];
+    Green = arr[1];
+    Blue = arr[0];
+
+}
 int comparator(const void *p, const void *q)
 {
 int l = *(const int *)p;
@@ -22,45 +58,4 @@ if (!(l&l))
 return -l;
 }
 
-int main()
-{
-        //DO NOT CHANGE WITHIN THIS AREA...
-        int red, blue, green;
-        cout<<"Enter Red, Green, and Blue values: ";
-        cin>>red>>green>>blue;
-        //...END OF "DO NOT CHANGE" AREA
 
-
-        // <-- ADD YOUR FUNCTION CALL HERE
-        trioValues( red, green, blue);
-
-      /*  //DO NOT CHANGE WITHIN THIS AREA...
-        cout<<"Rearranged....\n";
-        cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
-        return 0;
-        //...END OF "DO NOT CHANGE" AREA
-      */
-
-}
-
-// <-- ADD YOUR FUNCTION DEFINITON HERE
-int trioValues (int Red, int Green, int Blue)
-{
-   /* vector<int> numbers;
-
-    numbers.push_back (Red);
-    numbers.push_back (Green);
-    numbers.push_back (Blue);
-*/
-    int arr[] = {Red, Green, Blue};
-
-    int size = sizeof(arr)/sizeof(arr[0]);
-    qsort((void*)arr, size, sizeof(arr[0]), comparator);
-    Red = arr[2];
-    Green = arr[1];
-    Blue = arr[0];
-
-    cout<<"Rearragned....\n";
-    cout<<"RGB: "<<Red<<","<<Green<<","<<Blue<<endl;
-
-}
